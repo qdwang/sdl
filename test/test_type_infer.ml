d532379c0719dcc081596a8457577038f89836ca
qdwang <wangqidong@gmail.com>
2016-09-02 15:57:06 +0800

    get preparation for incremental parsing

--- test/test_type_infer.ml
+++ test/test_type_infer.ml
@@ -2,12 +2,34 @@ open Core.Std
 open Libparser.Lexer
 open Lexing
 open Pass
+open Libparser.Parser.MenhirInterpreter
+module S = MenhirLib.General
 
 let prepare content =
   (String.strip content) ^ "\n"
+  
+
+(* Preparing for incremental parsing *)
+let stack checkpoint =
+  match checkpoint with
+  | HandlingError env ->
+      stack env
+  | _ ->
+      assert false 
+
+let state checkpoint : int =
+  let result = Lazy.force (stack checkpoint) in 
+  match result with
+  | S.Nil ->
+      0
+  | S.Cons (Element (s, _, _, _), _) ->
+      number s
+(* Preparing for incremental parsing *)
+
 
 let test sample =
   let lexbuf = Lexing.from_string (prepare sample) in
+  (*let checkpoint = Libparser.Parser.Incremental.prog lexbuf.lex_curr_p in*)
   let result = try Libparser.Parser.prog Libparser.Lexer.read lexbuf with
     | SyntaxError msg -> print_endline msg; None
     | Libparser.Parser.Error -> print_endline "Parser Error:"; None
